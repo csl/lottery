c39bbcd9634032e5166d4382331cf0a2e2982e6f
shulong <shulong@shulong-laptop.(none)>
2011-03-09 22:48:08 +0800

    CHG: update 000-999

--- lottery.cpp
+++ lottery.cpp
@@ -1,15 +1,19 @@
 #include <string>
 #include <stdlib.h>
 #include <iostream>
+#include <sstream>
+#include <list>
 
 #define MAXDIGITS 49
-#define MD 99
+#define MD 100
+#define MDF 1000
 
 using namespace std;
 
+//00-99
 int num[MAXDIGITS]={0};
-int lottery[6]={0};
 int lotterytimes[MD]={0};
+int hlotterytimes[MDF]={0};
 int number=0;
 
 char* itoa(int number, char *buffer, int divisor)
@@ -60,6 +64,75 @@ char* itoa(int number, char *buffer, int divisor)
     return buffer;
 }
 
+void bubblesort(int *data, int n)
+{
+    int i, j, temp;
+    for (i = n - 1; i > 0; i--)
+    {
+        for (j = 0; j <= i - 1; j++)
+        {
+            if (data[j] > data[j + 1])
+            {
+                temp = data[j];
+                data[j] = data[j + 1];
+                data[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void countTnumber(string x, const char sep)
+{
+	int TNum[6]={0},TtNum[5]={0}, i=0, first=0;
+	int HNum[6]={0};
+	int O,T;
+	char temp[3]="", Htemp[4]="";
+	list<string> ret;
+    istringstream is(x);
+
+ 	first=0;
+    while(!is.eof())
+    {
+        string temp;
+        getline(is, temp, sep);
+		//omit 1
+		if (first != 0)
+		{
+			TNum[i] = atoi(temp.c_str());
+			i++;
+		}
+		first=1;
+    }
+
+	//Sorting
+	bubblesort(TNum, 6);
+	//printf("%d %d %d %d %d %d\n", TNum[0], TNum[1], TNum[2], TNum[3], TNum[4], TNum[5]);
+	//
+	//fetch T
+	for(int r=0; r<5;r++)
+	{
+		//fetch number
+		T = TNum[r]%10;
+		O = TNum[r+1]%10;
+
+		//combination
+		temp[0] = T + '0';
+		temp[1] = O + '0';
+		temp[2] = '\0';
+
+		//count
+		lotterytimes[atoi(temp)]++;
+	}
+
+	//000-999
+	//fetch 3~5 number
+    temp[0] = (TNum[2]%10) + '0';
+    temp[1] = (TNum[3]%10) + '0';
+    temp[2] = (TNum[4]%10) + '0';
+    temp[3] = '\0';
+	hlotterytimes[atoi(temp)]++;	
+}
+
 void combination(string x, int n, int r)
 {
 	char buffer[10];
@@ -67,8 +140,9 @@ void combination(string x, int n, int r)
 	if (r==n||r==0) {
 		for (int i=r;i>0;i--)
 			x= x +' ' + itoa(num[i-1],buffer,10);
-			//cout<<x<<endl;
 			
+			//cout<<x<<endl;
+			countTnumber(x, ' ');		
 			number++;
 			return;
 	}
@@ -80,6 +154,12 @@ void combination(string x, int n, int r)
 
 int main()
 {
+	FILE *fp;
+	FILE *fph;
+
+	fp = fopen("reslut.txt","w");	
+	fph = fopen("resluth.txt","w");	
+
 	for (int i=1; i<=MAXDIGITS; i++)
 	{
 		num[i-1] = i;
@@ -87,5 +167,32 @@ int main()
 
 	combination("", MAXDIGITS, 6);
 	cout << "total = " << number << endl;
+
+	//00-99
+	for (int i=0; i<MD; i++)
+	{
+		if (i==0)
+			fprintf(fp, "00: %d\r\n", lotterytimes[i]);
+		else if (i<10)
+			fprintf(fp, "0%d: %d\r\n", i, lotterytimes[i]);
+		else
+			fprintf(fp, "%d: %d\r\n", i, lotterytimes[i]);
+	}
+
+	//000-999
+	for (int i=0; i<MDF; i++)
+	{
+		if (i==0)
+			fprintf(fph, "000: %d\r\n", hlotterytimes[i]);
+		else if (i<10)
+			fprintf(fph, "00%d: %d\r\n", i, hlotterytimes[i]);
+		else if (i<100)
+			fprintf(fph, "0%d: %d\r\n", i, hlotterytimes[i]);
+		else
+			fprintf(fph, "%d: %d\r\n", i, hlotterytimes[i]);
+	}
+
+	fclose(fp);
+	fclose(fph);
 	return 0;
 }
